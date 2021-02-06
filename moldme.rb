8b8f22a7550c6e1a6bf5b5e834de1d3640bbfeb1
ButylFungi <75457856+ButylFungi@users.noreply.github.com>
2021-02-05 23:36:19 -0800

    Adding in Album Of the Year parsing

--- moldme.rb
+++ moldme.rb
@@ -4,18 +4,33 @@ require 'httparty'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-@url = "https://funkysouls.org/"
+@funkysouls_url = "https://funkysouls.org/"
+@album_of_the_year_url = "https://www.albumoftheyear.org/releases/this-week/"
 
 def parse_the_page(url)
 
 html = HTTParty.get(url)
 parsed = Nokogiri::HTML.parse(html)
 
-@doc = parsed.css('h2>a')
+if url == @funkysouls_url
+
+	@doc = parsed.css('h2>a')
+	
+elsif url == @album_of_the_year_url
+
+	#@doc = parsed.xpath('//*[@id="centerContent"]/div[1]/div[1]')
+	@doc = parsed.css('div>div>div>a>div') #Returns an array where the first one is the Artist Title and the second is the AlbumTitle.
+	
+end
+
 
 end
 
-def sort_for_list(parsed_document)
+def sort_for_list_album_of_the_year(parsed_document)
+#these come in as artist/album so they need to be paired up.
+end
+
+def sort_for_list_funkysouls(parsed_document)
 
 @doc.each do |s|
 	if (s.values[1].include?("[2021]") || s.values[1].include?("[2020]"))
@@ -32,11 +47,11 @@ def sort_for_list(parsed_document)
 end
 end
 
-def carve_in_rock(link, artist, title)
-File.open("list.txt", "a") {|f| f.write(link + "," + artist + "," + title + "\n") }
+def carve_in_rock(data, artist, title)
+File.open("list.txt", "a") {|f| f.write(data + "," + artist + "," + title + "\n") }
 end
 
-def next_phase()
+def next_phase_funkysouls()
 
 for i in 2..30
 	
@@ -47,11 +62,14 @@ next i
 end
 end
 
-parse_the_page(@url)
-sort_for_list(@doc)
-next_phase()
+parse_the_page(@funkysouls_url)
+sort_for_list_funkysouls(@doc)
+next_phase_funkysouls()
+
+
 
 #https://www.albumoftheyear.org/releases/this-week/
+#	#centerContent > div.flexContainer > div.wideLeft > div:nth-child(4) > a:nth-child(2) > div; //*[@id="centerContent"]/div[1]/div[1]/div[3]/a[1]/div
 #https://www.officialcharts.com/new-releases/
 #https://www.allmusic.com/newreleases
 #https://www.metacritic.com/browse/albums/release-date/new-releases/date
